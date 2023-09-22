4cbc57a2a642eda1c631cf7d7735a93fa9f1492d
svatsa159 <svatsa159@gmail.com>
2023-09-22 17:57:41 +0530

    API change

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-// export const API = 'https://bifrost-api-wxrn.onrender.com'
-export const API = 'http://192.168.1.3:8080'
+export const API = 'https://bifrost-api-wxrn.onrender.com'
+// export const API = 'http://192.168.1.3:8080'
