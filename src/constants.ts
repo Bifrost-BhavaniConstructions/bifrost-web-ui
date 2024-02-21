4522373f1d5eb68f969f5394616958d47cc67c79
svatsa159 <svatsa159@gmail.com>
2024-02-22 01:45:25 +0530

    ec2

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const API = "https://bifrost-api-wxrn.onrender.com";
+export const API = "http://ec2-65-2-178-53.ap-south-1.compute.amazonaws.com:80";
 // export const API = "http://localhost:8080";
