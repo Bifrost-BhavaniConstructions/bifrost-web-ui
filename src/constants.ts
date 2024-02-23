6666377b68911b1e52608d14030185a20084081e
svatsa159 <svatsa159@gmail.com>
2024-02-23 18:55:00 +0530

    ec2

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const API = "http://ec2-65-2-178-53.ap-south-1.compute.amazonaws.com:80";
+export const API = "https://bhavaniconstructions.co";
 // export const API = "http://localhost:8080";
